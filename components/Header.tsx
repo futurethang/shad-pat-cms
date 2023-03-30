9db388b440bf93fa8002c4996ac8192f1cb8b298
Kevin Hyde <kphyde@gmail.com>
2023-03-30 15:28:57 -0700

    img opt and poster size full height

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Nav from './Nav'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header>
       <h1>
-        <img src="./sp.gif" alt="Shadow Pattern" />
+        <Image src="./sp.gif" alt="Shadow Pattern" />
       </h1>
       <Nav />
     </header>
