9b4ae46e6d4dbf55a3147092d1a0b88a09b78613
Kevin Hyde <kphyde@gmail.com>
2023-03-31 11:55:26 -0700

    add seo and analytics basic configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,49 @@ import { Footer, Nav } from '../components'
 import UpcomingShows from '../components/UpcomingShows'
 import PastShows from '../components/PastShows'
 import Header from '../components/Header'
+import { DefaultSeo, NextSeo } from 'next-seo'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
+
+const SEO = {
+  title: 'Shadow Pattern - Seattle Rock and Roll Band',
+  description: 'Shadow Pattern is a five piece rock band from Seattle, WA. We play original music in a psychedelic, blues, and surf rock style.',
+  openGraph: {
+    type: 'music.band',
+    url: 'https://shadowpattern.com/',
+    title: 'Shadow Pattern - Seattle Rock and Roll Band',
+    description: 'Shadow Pattern is a five piece rock band from Seattle, WA. We play original music in a psychedelic, blues, and surf rock style.',
+    site_name: 'Shadow Pattern',
+    locale: 'en_US',
+    music: [
+      {
+        releaseDate: '2021-04-01',
+        album: 'Demonstration',
+        creator: 'Shadow Pattern',
+        song: 'Night Stroll',
+        track: 1,
+        disc: 1,
+        duration: 120,
+      },
+    ],
+    see_also: [
+      {
+        url: 'https://www.instagram.com/shadowpatternband/',
+      },
+      {
+        url: 'https://www.facebook.com/shadowpatternband/',
+      },
+    ],
+  },
+};
 
 const Home = () => {
   return (
     <div>
+      <GoogleAnalytics trackPageViews />
+      <DefaultSeo
+        {...SEO}
+      />
+      <p>Simple Usage</p>
       <Header />
       <main>
         <section className="shows">
